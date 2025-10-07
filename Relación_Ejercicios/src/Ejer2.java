e5738419023650297025e6f789f0cef0fe2f2805
MarggioRV <marggiorobles@hotmail.com>
2025-10-07 21:13:19 +0200

    + euros

--- Relación_Ejercicios/src/Ejer2.java
+++ Relación_Ejercicios/src/Ejer2.java
@@ -11,7 +11,6 @@ El sueldo semanal que le corresponde es de 720 euros
  */
 
 public class Ejer2 {
-
     public static void main(String[] args) {
 
         //Leer de teclado la altura y el radio
@@ -21,23 +20,23 @@ public class Ejer2 {
         System.out.println("Esc las horas hechas: ");
         int num1 = teclado.nextInt();
 
-         if (num1 <= 40) {/* Condicionales para cada formula  */
+         if (num1 <= 40) { /* Condicionales para cada formula  */
             int resultado1; /* Se debe declarar otra nueva variable */
             resultado1 = Formula1(num1);
-            System.out.println("El sueldo semanal que le corresponde es de: " + resultado1);
-        }
+            System.out.println("El sueldo semanal que le corresponde es de: " + resultado1 + " euros"); 
+        } /* Cada parte de la rpta se agrega con '+'' */
 
         else if (num1 > 40) {
             int resultado2; /* Lo dicho */
             resultado2 = Formula2(num1);
-            System.out.println("El sueldo semanal que le corresponde es de: " + resultado2);
+            System.out.println("El sueldo semanal que le corresponde es de: " + resultado2 + " euros");
         } 
       }
 
-    public static int Formula1(int num1) { /* F' para caso de < 40 */
+    public static int Formula1(int num1) { /* F' para caso de '< 40' */
     return 12*num1;}
 
-    public static int Formula2(int num1) { /* F' cuando sea 40 < , mulp solo el resto */
+    public static int Formula2(int num1) { /* F' cuando sea '40 <' , mulp solo el resto */
     return (40*12)+((num1-40)*16);}
 
 }
