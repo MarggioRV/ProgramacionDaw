0b574966bb5b97f542fa93ca3d02d366996ac3d1
MarggioRV <marggiorobles@hotmail.com>
2025-10-07 21:06:35 +0200

    Corrección pequeña, para evitar calculos grandes durante el testeo, usaba operaciones pequeñas. Me olvide cambiar x2 por la formula real.

--- Relación_Ejercicios/src/Ejer2.java
+++ Relación_Ejercicios/src/Ejer2.java
@@ -14,28 +14,28 @@ public class Ejer2 {
 
     public static void main(String[] args) {
 
-    //Leer de teclado la altura y el radio
-    Scanner teclado=new Scanner(System.in);
+        //Leer de teclado la altura y el radio
+        Scanner teclado=new Scanner(System.in);
 
-    //Entrada
-    System.out.println("Esc las horas hechas: ");
-    int num1 = teclado.nextInt();
+        //Entrada
+        System.out.println("Esc las horas hechas: ");
+        int num1 = teclado.nextInt();
 
-     if (num1 <= 40) {/* Condicionales para cada formula  */
-        int resultado1; /* Se debe declarar otra nueva variable */
-        resultado1 = Formula1(num1);
-        System.out.println("El sueldo semanal que le corresponde es de: " + resultado1);
-    }
+         if (num1 <= 40) {/* Condicionales para cada formula  */
+            int resultado1; /* Se debe declarar otra nueva variable */
+            resultado1 = Formula1(num1);
+            System.out.println("El sueldo semanal que le corresponde es de: " + resultado1);
+        }
 
-    else if (num1 > 40) {
-        int resultado2; /* Lo dicho */
-        resultado2 = Formula2(num1);
-        System.out.println("El sueldo semanal que le corresponde es de: " + resultado2);
-    } 
-    }
+        else if (num1 > 40) {
+            int resultado2; /* Lo dicho */
+            resultado2 = Formula2(num1);
+            System.out.println("El sueldo semanal que le corresponde es de: " + resultado2);
+        } 
+      }
 
     public static int Formula1(int num1) { /* F' para caso de < 40 */
-    return 2*num1;}
+    return 12*num1;}
 
     public static int Formula2(int num1) { /* F' cuando sea 40 < , mulp solo el resto */
     return (40*12)+((num1-40)*16);}
