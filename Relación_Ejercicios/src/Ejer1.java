fc6ed2c07d4977dd3b070edc5964cee48d137cc8
MarggioRV <marggiorobles@hotmail.com>
2025-10-07 14:26:21 +0200

    Mensaje a ti mismo
    -E: Cmbiar dia los objetos LocalDateTime, pues sino no funcionara nada debido a las fechas vencidas.
    
    -En cuanto al ejercicio 2, recuerda las logica de las formulas se resuelve más rapido en papel.

--- Relación_Ejercicios/src/Ejer1.java
+++ Relación_Ejercicios/src/Ejer1.java
@@ -8,38 +8,33 @@ según la hora actual. Se utilizarán los tramos de 6 a 12, de 13 a 20 y de 21 a
 public class Ejer1 {
     public static void main(String[] args){
 
-    LocalDateTime hora = LocalDateTime.now();
-    System.out.println("El tiempo es: " + hora);
-
-    // Extraer solo la hora 
-    LocalTime Solohora = hora.toLocalTime();
-    System.out.println("La hora es: " + Solohora);
+    LocalDateTime ahora = LocalDateTime.now();
+    System.out.println("El tiempo es: " + ahora);
 
     // Objetos tiempo, de LocalTime
-    LocalTime MñInicio = LocalTime.of(6, 0);
-    LocalTime MñFin = LocalTime.of(12, 0);
-    LocalTime TdInicio = LocalTime.of(13, 0);
-    LocalTime TdFin = LocalTime.of(20, 0);
-    LocalTime NhInicio = LocalTime.of(21, 0);
-    LocalTime NhFin = LocalTime.of(5, 59);
-
+    LocalDateTime MñInicio = LocalDateTime.of(2025, 10, 7, 6, 0, 0);
+    LocalDateTime MñFin = LocalDateTime.of(2025, 10, 7,12, 0);
+    LocalDateTime TdInicio = LocalDateTime.of(2025, 10, 7,13, 0);
+    LocalDateTime TdFin = LocalDateTime.of(2025, 10, 7,20, 0);
+    LocalDateTime NhInicio = LocalDateTime.of(2025, 10, 7,21, 0);
+    LocalDateTime NhFin = LocalDateTime.of(2025, 10, 7,5, 59);
 
    // consicionales if entre horas
    // Logica: 
    // x.isBefore(y) = y es antes de x
    // x.isAfter(y) = y es despues de x
 
-    if (!Solohora.isBefore(MñInicio) && !Solohora.isAfter(MñFin)) 
+    if (!ahora.isBefore(MñInicio) && !ahora.isAfter(MñFin)) 
     {System.out.println("¡Buenos días!");}
 
-    if (!Solohora.isBefore(TdInicio) && !Solohora.isAfter(TdFin)) 
-    {System.out.println("¡Buenos Tardes!");}
+    if (!ahora.isBefore(TdInicio) && !ahora.isAfter(TdFin)) 
+    {System.out.println("¡Buenas tardes!");}
     
-    else if (!Solohora.isBefore(NhInicio) && !Solohora.isAfter(NhFin)) 
+    else if (!ahora.isBefore(NhInicio) && !ahora.isAfter(NhFin)) 
     {System.out.println("¡Buenas noches!");}
 
     else {
-        System.out.println("¡Buenas Nada! Toca esperar....");
+        System.out.println("¡Buenas nada! Toca esperar....");
     }
   }
 }
--- Relación_Ejercicios/src/Ejer2.java
+++ Relación_Ejercicios/src/Ejer2.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 /* 2. Escribe un programa que calcule el salario semanal de un trabajador teniendo en cuenta 
 que las horas ordinarias (40 primeras horas de trabajo) se pagan a 12 euros la hora. 
@@ -10,5 +11,36 @@ El sueldo semanal que le corresponde es de 720 euros
  */
 
 public class Ejer2 {
-    
+
+    public static void main(String[] args) {
+
+    //Leer de teclado la altura y el radio
+    Scanner teclado=new Scanner(System.in);
+
+    //Entrada
+    System.out.println("Esc las horas hechas: ");
+    int num1 = teclado.nextInt();
+
+     if (num1 <= 40) {/* Condicionales para cada formula  */
+        int resultado1; /* Se debe declarar otra nueva variable */
+        resultado1 = Formula1(num1);
+        System.out.println("El sueldo semanal que le corresponde es de: " + resultado1);
+    }
+
+    else if (num1 > 40) {
+        int resultado2; /* Lo dicho */
+        resultado2 = Formula2(num1);
+        System.out.println("El sueldo semanal que le corresponde es de: " + resultado2);
+    } 
+    }
+
+    public static int Formula1(int num1) { /* F' para caso de < 40 */
+    return 2*num1;}
+
+    public static int Formula2(int num1) { /* F' cuando sea 40 < , mulp solo el resto */
+    return (40*12)+((num1-40)*16);}
+
 }
+
+
+
