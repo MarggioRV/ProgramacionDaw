6424e5fe5517e702bc5c34dcc70b5ff3d9536405
MarggioRV <marggiorobles@hotmail.com>
2025-11-09 23:18:21 +0100

    Tuve un erroe extraño con java, el largo de las matrices tambien puede obtenerse por el ".length", y el la impresion mejora con el manejo del index.

---
+++ Relación_Ejercicios3/src/Ejer6.java
@@ -0,0 +1,58 @@
+/* 6. Escribir una función que dadas dos matrices devuelve la matriz donde la primera columna es la 
+suma, la segunda columna el producto y la tercera columna la media de las correspondientes columnas de
+las matrices pasadas como argumento. */
+
+public class Ejer6 {
+    public static void main(String[] args) {
+
+        //Declaración de matrices 
+
+        double[][] matriz1 = {
+            {1, 2, 3}, {4, 5, 6}
+        };
+
+        double[][] matriz2 = {
+            {7, 8, 9}, {10, 11, 12}
+        };
+
+        //Obtener calculo_filas
+        int filas = matriz1.length;
+        int columnas = matriz1[0].length;
+
+        //Llamada
+        double[][] rpta = combinarMatrices(matriz1, matriz2, filas, columnas);
+
+        //Imprimir 
+        for (int i = 0; i < rpta.length; i++) {
+            System.out.println("Fila " + i + ":");
+            System.out.println("  Suma: " + rpta[i][0]);
+            System.out.println("  Producto: " + rpta[i][1]);
+            System.out.println("  Media: " + rpta[i][2]);
+        }
+
+    }
+
+    //Función combinadora
+    public static double[][] combinarMatrices(double[][] m1, double[][] m2, int filas, int columnas) {
+        double[][] rpta = new double[filas][3]; /* Exactamente, las 3 */
+
+        for (int i = 0; i < filas; i++) {
+            double suma = 0;
+            double producto = 0;
+            double media = 0;
+
+            for (int j = 0; j < columnas; j++) {
+                suma += m1[i][j] + m2[i][j];
+                producto *= m1[i][j] * m2[i][j];
+                media += (m1[i][j] + m2[i][j]) / 2.0;
+            }
+
+            rpta[i][0] = suma;
+            rpta[i][1] = producto;
+            rpta[i][2] = media;
+        }
+
+        return rpta;
+    }
+  
+}
