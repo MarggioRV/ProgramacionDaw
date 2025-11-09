1caac67306b64b8daf09bc3f07be0f1fac1c4e71
MarggioRV <marggiorobles@hotmail.com>
2025-11-09 22:40:17 +0100

    Reemplace las lineas dadas para con arrayllISt, tambien quite la funcion push, pues parece ser aqui no es necesaria. Cosa menor, pero probe a ver for each.

--- Relación_Ejercicios3/src/Ejer5.java
+++ Relación_Ejercicios3/src/Ejer5.java
@@ -1,14 +1,15 @@
 /* El 4, pero con ArrayList<Integer> */
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejer5 {
     public static void main(String[] args) {
         //Crear el vector dinamico
-        int[] vector=null;
+        ArrayList<Integer> vector = new ArrayList<>();
         //Loteria
-        int[] LOTERIA={10,20,30,40,50,60};
+        ArrayList<Integer> LOTERIA = new ArrayList<>(Arrays.asList(10, 20, 30, 40, 50, 60));
         
         //Insertar un Valores
         Scanner teclado=new Scanner(System.in);
@@ -16,60 +17,36 @@ public class Ejer5 {
         
         //For que solo permite 6 valores
         for (int i = 0; i < 6; i++) {
-        vector = push(vector, teclado.nextInt());
+        vector.add(teclado.nextInt()); //Con add, se añade directamente
         }
         
         //Sin Ordenar 
         System.out.println("Tus números son: ");
-            for(int i=0;i<vector.length;i++){
-            System.out.print(vector[i]+" "); //Automatiza la impresión
+        /* for each solo salla en ordenamiento, se puede usar*/
+        for (int num : vector) {
+             System.out.print(num + " ");
         }
         
         //Comprobación Inicial
-        if(Arrays.equals(LOTERIA, vector)){ /* Comparación Literal */
-            System.out.println();
-            System.out.println("\n¡Gamaste! Acertaste todos los #s");
-        //Aplicación del conteo
+        if (vector.equals(LOTERIA)) { /* Aqui, rg true si tienen los mismos elementos en el mismo orden */
+            System.out.println("\n¡Ganaste! Acertaste todos los números en orden.");
         } else if (ContarNRepetidos(vector, LOTERIA) == 6) {
-            System.out.println();
-            System.out.println("\n¡Ganaste! Aunque los #s no estan orden correcto...");
-        }else{
-            System.out.println();
+            System.out.println("\n¡Ganaste! Todos los números acertados, pero el orden no coincide.");
+        } else {
             System.out.println("\nAcertaste: " + ContarNRepetidos(vector, LOTERIA) + " número(s)");
-        } 
-
-        teclado.close();
-    }
-     
-    //Función push
-    public static int[] push(int[] array, int valor){
-        int longitud;
-        if (array==null) {longitud=0;}
-        else {longitud=array.length;}
-        //Crea un array con +1 de tamaño
-        int[] salida=new int[longitud+1];
-        //copiar lo que has esñ ese momento 
-        for (int i=0;i<longitud;i++){
-            salida[i]=array[i];
         }
-        salida[longitud]=valor;
-        return salida;
+        teclado.close();
     }
     
     //Contador_NRepetidos
-    public static int ContarNRepetidos(int[] vector, int[] LOTERIA) {
-        int contador = 0;
-        
-        /* 2 recorridos paralelos */
-        for (int i=0; i<vector.length; i++) { 
-            for (int j=0; j<LOTERIA.length; j++) {
-                /* Comparamos de 1 en 1 */
-                if (vector[i] == LOTERIA[j]) {
-                    contador++;
-                }
-            }
+    public static int ContarNRepetidos(ArrayList<Integer> vector, ArrayList<Integer> LOTERIA) {
+    int contador=0;
+    for (int num : vector) {
+        if (LOTERIA.contains(num)) {
+            contador++;
         }
-        return contador;
+    }
+    return contador;
     }
 
 }
